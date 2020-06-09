f30a66252714ec170d9b8b03540bf3467b50f373
Igor Slootsky <iggy555@hotmail.com>
2020-06-08 20:59:14 -0400

    adding first .py file

---
+++ test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 00:37:18 2020
+
+@author: Igor
+"""
+
+
+
+# =============================================================================
+# import numpy as np
+# 
+# print (dir(np))
+# 
+# np.digitize(5,2)
+# =============================================================================
+
+# print ('whats up bro')
+
+# good = input('put you number here fool: ')
+
+# print (good + 'n9ne6')
+
+# def hello():
+#     """ prints bye world 
+#     """
+#     print ('bye world')
+    
+import random
+print (random.randint(1,10))
+
+
